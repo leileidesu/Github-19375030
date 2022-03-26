8e1de7b3c2b37946f372f5c3d41303370ac563ab
xiao7 <3499475017@qq.com>
2022-03-26 19:29:50 +0800

    lab2

---
+++ 附件/index.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+    if(Kernal.isLogin()) {
+        initUserInfo();
+    }
+
+    // 设置监听器，点击搜索按钮后，执行对应函数
+    document.getElementById('search-btn').addEventListener('click', function() {
+        search();
+    });
+
+    // TODO: 在此为 top-right 元素设置监听器
+    // document.getElementById('top-right') ...
+    document.getElementById('top-right').addEventListener('click', function() {
+        clickLogin();
+    });
+}
+
+function search() {
+    // TODO: 搜索触发后的行为
+    
+    var keyword=document.getElementsByTagName("input")[0].value;
+    console.log(keyword);
+    if(keyword=="")
+    {
+        window.alert("请输入搜索内容");
+    }
+    else
+    {
+        window.alert(keyword);
+    }
+}
+
+function clickLogin() {
+    if(!Kernal.isLogin()) {
+        login();
+    }
+    else {
+        logout();
+    }
+}
+
+function initUserInfo() {
+    // TODO: 修改页面显示错误的 bug，另外注意图片路径是否正确
+    var username = Kernal.getUserName();
+    console.log(username);
+    var content = '<div id="user">\
+                        <span id="user-img">\
+                            <img src="img/user.jpg" />\
+                        </span>\
+                        <span id="name">' + username.slice(0,5) + '</span>\
+                    </div>';
+    document.getElementById('top-right').innerHTML = content;
+}
+
+// ============================================================ 你不需要去关注的代码
+
+function login() {
+    Kernal.login();
+    location.reload();
+}
+
+function logout() {
+    Kernal.logout();
+    location.reload();
+}
+
